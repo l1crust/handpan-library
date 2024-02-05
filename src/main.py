60002a10c2b9920b504aa77321e20519667f5c87
admin <l1crust@protonmail.com>
2024-02-05 18:22:06 +0100

    base project

---
+++ src/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from store import save_youtube_to_csv, save_to_csv, get_scales_from_csv, get_entries_for_scale
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	scales = get_scales_from_csv()
+	return render_template('index.html', scales=scales)
+
+@app.route('/manage', methods=['GET', 'POST'])
+def manage():
+    if request.method == 'POST':
+        # Handle the form data here
+        link = request.form.get('link')
+        scale = request.form.get('scales')
+        notes = request.form.get('notes')
+        tags = request.form.get('tags')
+
+        # Do something with the form data (e.g., save to a database)
+        # For now, let's just print them
+        print(f"Link: {link}, Scale: {scale}, Notes: {notes}, Tags: {tags}")
+        save_youtube_to_csv(link, scale, notes, tags)
+
+    return render_template('manage.html')
+
+
+@app.route('/scale/<scale>')
+def scale_entries(scale):
+    entries = get_entries_for_scale(scale)
+    return render_template('scale_entries.html', scale=scale, entries=entries)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True)
---
+++ src/store.py
@@ -0,0 +1,41 @@
+import csv
+
+def youtube_id_to_url(videoId):
+	if videoId.isalnum():
+		return "http://www.youtube.com/embed/" + videoId
+	else:  return ""
+
+def save_youtube_to_csv(videoId, scale, notes, tags, filename='music_data.csv'):
+	save_to_csv(youtube_id_to_url(videoId), scale, notes, tags, filename=filename)
+
+def save_to_csv(url, scale, notes, tags, filename='music_data.csv'):
+	if url != "" and scale != "":
+	    with open(filename, mode='a', newline='') as file:
+	        writer = csv.writer(file)
+	        writer.writerow([url, scale, notes, tags])
+	else:
+		print("error")
+
+
+def get_scales_from_csv(filename='music_data.csv'):
+    scales = set()
+    with open(filename, mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            scales.add(row[1])  # Assuming scale is stored in the second column
+    orderedScales = sorted(list(scales))
+    return orderedScales
+
+def get_entries_for_scale(scale, filename='music_data.csv'):
+    entries = []
+    with open(filename, mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[1] == scale:  # Check if the scale matches
+                entries.append({
+                    'link': row[0],
+                    'scale': row[1],
+                    'notes': row[2],
+                    'tags': row[3]
+                })
+    return entries
